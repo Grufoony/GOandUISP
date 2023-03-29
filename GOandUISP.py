5d54e63512ea7d3fe0d8321b4917b3117267a9d2
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-03-30 00:10:59 +0200

    enhanced code and comments

--- GOandUISP.py
+++ GOandUISP.py
@@ -12,12 +12,11 @@ input_file = input_file[['Name', 'Year', 'Sex', 'Style', 'Distance', 'Time', 'Te
 # replacing style names
 input_file = input_file.replace({'Style': styles})
 input_file['Race'] = input_file['Distance'].astype(str) + " " + input_file['Style']
-# groupby races and times
+# groupby races and times, i.e. get unique athletes
 input_file = input_file.groupby(['Name', 'Year', 'Sex', 'Team'])[['Race', 'Time']].agg(list)
 
+# creates empty output database with columns' names
 out_columns = ['Cognome', 'Nome', 'Anno', 'Sesso']
-output_file = pd.DataFrame(columns=out_columns)
-
 max_len = input_file['Race'].apply(len).max()
 for i in range(max_len):
     out_columns.append('Gara' + str(i+1))
@@ -27,12 +26,13 @@ output_file = pd.DataFrame(columns=out_columns)
 
 output_file['Anno'] = input_file.index.get_level_values('Year')
 output_file['Sesso'] = input_file.index.get_level_values('Sex')
+output_file['Societa'] = input_file.index.get_level_values('Team')
+
 # get unique athletes
 athletes = input_file.index
 
-# split name column into words and ask surname by input_file if number of words greater than 2
+# split name column into words and ask name/surname by input if number of words is greater than 2
 for index, full_name in zip(range(len(athletes)), athletes.get_level_values('Name')):
-    # print(athletes[index])
     if len(full_name.split()) > 2:
         print("Inserisci i dati di " + str(athletes[index]) + ": ")
         surname = input("Inserisci il cognome: ")
@@ -49,6 +49,5 @@ for index, race, time in zip(range(len(input_file['Race'])), input_file['Race'] 
         output_file.loc[index, 'Gara' + str(i+1)] = race[i]
         output_file.loc[index, 'Tempo' + str(i+1)] = time[i]
 
-output_file['Societa'] = input_file.index.get_level_values('Team')
 # print output_file on xlsx file
 output_file.to_excel("output.xlsx", index=False)
