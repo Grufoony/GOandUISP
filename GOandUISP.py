6379c7a2fc66c9f6e765cdbb4abed75ad4e377ae
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-03-30 00:02:44 +0200

    fix #1

--- GOandUISP.py
+++ GOandUISP.py
@@ -2,42 +2,53 @@ import pandas as pd
 # races dictionary: GoAndSwim -> dbMeeting
 styles = { 'F':'Delfino', 'D':'Dorso', 'R':'Rana', 'S':'SL'}
 in_columns = ['Name', 'Year', 'Sex', '', 'Distance', 'Style', 'Team'] + [''] * 3 + ['Time'] + [''] * 2 + ['Boolean'] + [''] * 6
-input = pd.read_excel("input.xlsx")
-input.columns=in_columns
+input_file = pd.read_excel("input.xlsx")
+input_file.columns=in_columns
 # keep only rows with boolean set to T (valid times) and strip spaces in style column
-input.drop(input.loc[input['Boolean'].str.strip() != 'T'].index, inplace=True)
-input['Style'] = input['Style'].str.strip()
+input_file.drop(input_file.loc[input_file['Boolean'].str.strip() != 'T'].index, inplace=True)
+input_file['Style'] = input_file['Style'].str.strip()
 #keeping only interesting data
-input = input[['Name', 'Year', 'Sex', 'Style', 'Distance', 'Time', 'Team']]
+input_file = input_file[['Name', 'Year', 'Sex', 'Style', 'Distance', 'Time', 'Team']]
 # replacing style names
-input = input.replace({'Style': styles})
-input['Race'] = input['Distance'].astype(str) + " " + input['Style']
+input_file = input_file.replace({'Style': styles})
+input_file['Race'] = input_file['Distance'].astype(str) + " " + input_file['Style']
 # groupby races and times
-input = input.groupby(['Name', 'Year', 'Sex', 'Team'])[['Race', 'Time']].agg(list)
+input_file = input_file.groupby(['Name', 'Year', 'Sex', 'Team'])[['Race', 'Time']].agg(list)
 
 out_columns = ['Cognome', 'Nome', 'Anno', 'Sesso']
-output = pd.DataFrame(columns=out_columns)
+output_file = pd.DataFrame(columns=out_columns)
 
-max_len = input['Race'].apply(len).max()
+max_len = input_file['Race'].apply(len).max()
 for i in range(max_len):
     out_columns.append('Gara' + str(i+1))
     out_columns.append('Tempo' + str(i+1))
 out_columns.append('Societa')
+output_file = pd.DataFrame(columns=out_columns)
 
-# split name column into name and surname keeping only the first two words
-name_column = input.index.get_level_values('Name')
-name_column = name_column.str.split(expand=True, n=2)
-output['Nome'] = name_column.get_level_values(1)
-output['Cognome'] = name_column.get_level_values(0)
+output_file['Anno'] = input_file.index.get_level_values('Year')
+output_file['Sesso'] = input_file.index.get_level_values('Sex')
+# get unique athletes
+athletes = input_file.index
 
-output['Anno'] = input.index.get_level_values('Year')
-output['Sesso'] = input.index.get_level_values('Sex')
+# split name column into words and ask surname by input_file if number of words greater than 2
+for index, full_name in zip(range(len(athletes)), athletes.get_level_values('Name')):
+    # print(athletes[index])
+    if len(full_name.split()) > 2:
+        print("Inserisci i dati di " + str(athletes[index]) + ": ")
+        surname = input("Inserisci il cognome: ")
+        name = input("Inserisci il nome: ")
+        output_file.loc[index, 'Nome'] = name.upper()
+        output_file.loc[index, 'Cognome'] = surname.upper()
+    else:
+        name_column = full_name.split()
+        output_file.loc[index, 'Nome'] = name_column[1].upper()
+        output_file.loc[index, 'Cognome'] = name_column[0].upper()
 
-for index, race, time in zip(range(len(input['Race'])), input['Race'] , input['Time']):
+for index, race, time in zip(range(len(input_file['Race'])), input_file['Race'] , input_file['Time']):
     for i in range(len(race)):
-        output.loc[index, 'Gara' + str(i+1)] = race[i]
-        output.loc[index, 'Tempo' + str(i+1)] = time[i]
+        output_file.loc[index, 'Gara' + str(i+1)] = race[i]
+        output_file.loc[index, 'Tempo' + str(i+1)] = time[i]
 
-output['Societa'] = input.index.get_level_values('Team')
-# print output on xlsx file
-output.to_excel("output.xlsx", index=False)
+output_file['Societa'] = input_file.index.get_level_values('Team')
+# print output_file on xlsx file
+output_file.to_excel("output.xlsx", index=False)
