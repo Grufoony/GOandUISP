7987c3ca0950d45cf4263d9ba3e119121e8fb11c
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-04-29 13:46:02 +0200

    more files

--- GOandUISP.py
+++ GOandUISP.py
@@ -1,73 +1,81 @@
-__version__ = "1.2.1"
-# races dictionary: GoAndSwim -> dbMeeting
-styles = { 'F':'Delfino', 'D':'Dorso', 'R':'Rana', 'S':'SL'}
-in_columns = ['Name', 'Year', 'Sex', '', 'Distance', 'Style', 'Team'] + [''] * 3 + ['Time'] + [''] * 2 + ['Boolean', 'Absent'] + [''] * 5
-
-print("GOandUISP v" + __version__ + " by Gregorio Berselli.")
-print("Per informazioni su come utilizzare il programma si consulti la repository GitHub: https://github.com/Grufoony/GOandUISP\n\n")
-
 import pandas as pd
-input_file = pd.read_excel("input.xlsx")
-input_file.columns=in_columns
+import os
+__version__ = "1.3.0"
+# races dictionary: GoAndSwim -> dbMeeting
+__styles__ = { 'F':'Delfino', 'D':'Dorso', 'R':'Rana', 'S':'SL'}
+__in_columns__ = ['Name', 'Year', 'Sex', '', 'Distance', 'Style', 'Team'] + [''] * 3 + ['Time'] + [''] * 2 + ['Boolean', 'Absent'] + [''] * 5
 
-# now print how many athletes are in each team and the total (partecipating medals)
-counter_df = input_file.drop(input_file.loc[input_file['Absent'].str.strip() == 'A'].index, inplace=False)
-counter_df = counter_df.groupby(['Name', 'Year', 'Sex', 'Team'])[['Time']].agg(list)
+def convert(file_name):
+    input_file = pd.read_excel(file_name)
+    input_file.columns=__in_columns__
 
-print(counter_df.index.get_level_values('Team').value_counts())
-print("TOTALE ATLETI PARTECIPANTI: " + str(len(counter_df.index)))
+    # now print how many athletes are in each team and the total (partecipating medals)
+    counter_df = input_file.drop(input_file.loc[input_file['Absent'].str.strip() == 'A'].index, inplace=False)
+    counter_df = counter_df.groupby(['Name', 'Year', 'Sex', 'Team'])[['Time']].agg(list)
 
-input("Premere un tasto qualsiasi per continuare...")
+    print(counter_df.index.get_level_values('Team').value_counts())
+    print("TOTALE ATLETI PARTECIPANTI: " + str(len(counter_df.index)))
 
-# keep only rows with boolean set to T (valid times) and strip spaces in style column
-input_file.drop(input_file.loc[input_file['Boolean'].str.strip() != 'T'].index, inplace=True)
-input_file['Style'] = input_file['Style'].str.strip()
-#keeping only interesting data
-input_file = input_file[['Name', 'Year', 'Sex', 'Style', 'Distance', 'Time', 'Team']]
-# replacing style names
-input_file = input_file.replace({'Style': styles})
-input_file['Race'] = input_file['Distance'].astype(str) + " " + input_file['Style']
-# groupby races and times, i.e. get unique athletes
-input_file = input_file.groupby(['Name', 'Year', 'Sex', 'Team'])[['Race', 'Time']].agg(list)
+    input("Premere un tasto qualsiasi per continuare...")
 
-# creates empty output database with columns' names
-out_columns = ['Cognome', 'Nome', 'Anno', 'Sesso']
-max_len = input_file['Race'].apply(len).max()
-for i in range(max_len):
-    out_columns.append('Gara' + str(i+1))
-    out_columns.append('Tempo' + str(i+1))
-out_columns.append('Societa')
-output_file = pd.DataFrame(columns=out_columns)
+    # keep only rows with boolean set to T (valid times) and strip spaces in style column
+    input_file.drop(input_file.loc[input_file['Boolean'].str.strip() != 'T'].index, inplace=True)
+    input_file['Style'] = input_file['Style'].str.strip()
+    #keeping only interesting data
+    input_file = input_file[['Name', 'Year', 'Sex', 'Style', 'Distance', 'Time', 'Team']]
+    # replacing style names
+    input_file = input_file.replace({'Style': __styles__})
+    input_file['Race'] = input_file['Distance'].astype(str) + " " + input_file['Style']
+    # groupby races and times, i.e. get unique athletes
+    input_file = input_file.groupby(['Name', 'Year', 'Sex', 'Team'])[['Race', 'Time']].agg(list)
 
-output_file['Anno'] = input_file.index.get_level_values('Year')
-output_file['Sesso'] = input_file.index.get_level_values('Sex')
-output_file['Societa'] = input_file.index.get_level_values('Team')
+    # creates empty output database with columns' names
+    out_columns = ['Cognome', 'Nome', 'Anno', 'Sesso']
+    max_len = input_file['Race'].apply(len).max()
+    for i in range(max_len):
+        out_columns.append('Gara' + str(i+1))
+        out_columns.append('Tempo' + str(i+1))
+    out_columns.append('Societa')
+    output_file = pd.DataFrame(columns=out_columns)
 
-# get unique athletes
-athletes = input_file.index
+    output_file['Anno'] = input_file.index.get_level_values('Year')
+    output_file['Sesso'] = input_file.index.get_level_values('Sex')
+    output_file['Societa'] = input_file.index.get_level_values('Team')
 
-# split name column into words and ask surname in input if the number of words is greater than 2
-for index, full_name in enumerate(athletes.get_level_values('Name')):
-    if len(full_name.split()) > 2:
-        print("Inserisci i dati di " + str(athletes[index]) + ": ")
-        while True:
-            surname = input("Inserisci il COGNOME: ").upper()
-            if surname in full_name.upper():
-                name = full_name.upper().replace(surname + ' ', '')
-                break
-            else:
-                print("COGNOME non presente nel nome, riprova: ")
-        output_file.loc[index, 'Nome'] = name
-        output_file.loc[index, 'Cognome'] = surname
-    else:
-        name_column = full_name.split()
-        output_file.loc[index, 'Nome'] = name_column[1].upper()
-        output_file.loc[index, 'Cognome'] = name_column[0].upper()
+    # get unique athletes
+    athletes = input_file.index
 
-for index, race, time in zip(range(len(input_file['Race'])), input_file['Race'] , input_file['Time']):
-    for i in range(len(race)):
-        output_file.loc[index, 'Gara' + str(i+1)] = race[i]
-        output_file.loc[index, 'Tempo' + str(i+1)] = time[i]
+    # split name column into words and ask surname in input if the number of words is greater than 2
+    for index, full_name in enumerate(athletes.get_level_values('Name')):
+        if len(full_name.split()) > 2:
+            print("Inserisci i dati di " + str(athletes[index]) + ": ")
+            while True:
+                surname = input("Inserisci il COGNOME: ").upper()
+                if surname in full_name.upper():
+                    name = full_name.upper().replace(surname + ' ', '')
+                    break
+                else:
+                    print("COGNOME non presente nel nome, riprova: ")
+            output_file.loc[index, 'Nome'] = name
+            output_file.loc[index, 'Cognome'] = surname
+        else:
+            name_column = full_name.split()
+            output_file.loc[index, 'Nome'] = name_column[1].upper()
+            output_file.loc[index, 'Cognome'] = name_column[0].upper()
 
-# print output_file on xlsx file
-output_file.to_excel("output.xlsx", index=False)
+    for index, race, time in zip(range(len(input_file['Race'])), input_file['Race'] , input_file['Time']):
+        for i in range(len(race)):
+            output_file.loc[index, 'Gara' + str(i+1)] = race[i]
+            output_file.loc[index, 'Tempo' + str(i+1)] = time[i]
+
+    # print output_file on xlsx file
+    output_file.to_excel(file_name, index=False)
+
+if __name__ == "__main__":
+    print("GOandUISP v" + __version__ + " by Gregorio Berselli.")
+    print("Per informazioni su come utilizzare il programma si consulti la repository GitHub: https://github.com/Grufoony/GOandUISP\n\n")
+    for f in os.listdir():
+        if f.endswith(".xlsx"):
+            convert(f)
+            print("File " + f + " convertito con successo!")
+    print("Tutti i file presenti nella cartella sono stati convertiti con successo!")
--- README.md
+++ README.md
@@ -4,18 +4,18 @@ Script Python per la conversione di file _.xlsx_ da output GoAndSwim a input per
 ## Guida rapida
 
 ### Ottenere l'eseguibile
-Per utilizzare il programma è necessario ottenere l'eseguibile _GOandUISP.exe_: [scrivimi una mail](mailto:gregorio.berselli@studio.unibo.it).
+Lo script si può eseguire sulla piattaforma [Google Colab](https://colab.research.google.com/github/Grufoony/GOandUISP/blob/main/GOandUISP.ipynb).
 
-Un'alternativa all'utilizzo dell'eseguibile (che a Windows di base non piace) è [Google Colab](https://colab.research.google.com/github/Grufoony/GOandUISP/blob/main/GOandUISP.ipynb)
+Per qualsiasi problema [scrivimi una mail](mailto:gregorio.berselli@studio.unibo.it).
 
 ### Preparare il file di input
-1. Creare un file excel (_.xlsx_) chiamato _input_, e aprirlo con Excel. Questo file deve essere vuoto per il momento
-2. Una volta aperto il file _input.xlsx_, aprire il file _.GAS_ della manifestazione che si vuole convertire
+1. Creare un file excel (_.xlsx_), e aprirlo con Excel. Questo file deve essere vuoto per il momento
+2. Una volta aperto il file, aprire il file _.GAS_ della manifestazione che si vuole convertire
 3. Selezionare le gare di interesse ed entrare in esse (tasto in basso a sinistra di GoAndSwim)
 4. Senza dover fare nulla, si dovrebbe essere sul primo atleta dell'elenco. A questo punto eseguire la combinazione di tasti _CTRL + C_ (copia)
-5. Andare nel file vuoto _input.xlsx_ nella prima cella in alto a sinistra (la A1) ed eseguirle la combinazione di tasti _CTRL + V_ (incolla). In questo modo tutte le gare di interesse dovrebbero essere state copiate nel file Excel
-6. Salvare il file _input.xlsx_ e chiuderlo
+5. Andare nel file vuoto appena creato nella prima cella in alto a sinistra (la A1) ed eseguirle la combinazione di tasti _CTRL + V_ (incolla). In questo modo tutte le gare di interesse dovrebbero essere state copiate nel file Excel
+6. Salvare il file e chiuderlo
 
-Se è stato fatto tutto correttamente, basterà eseguire (doppio click sinistro) il file _GOandUISP.exe_ __nella stessa cartella__ del file _input.xlsx_: dopo qualche secondo verrà creato un file _output.xlsx_ con i dati formattati nel modo desiderato.
+Se è stato fatto tutto correttamente, basterà eseguire lo script _GOandUISP.py_ __nella stessa cartella__ del file: questo verrà sostituito con il file formattato correttamente.
 
 __NOTA__: nel caso di atleti con più di un nome/cognome il programma chiederà di inserirli manualmente, comunicandovi tutti i dati che possiede di quel determinato atleta. Potete scrivere sia in maiuscolo che in minuscolo ed inserire spazi (per i nomi/cognomi multipli). Una volta inserito il nome/cognome multiplo richiesto, premere INVIO e ripetere l'azione ogni qualvolta richiesta dal programma. Al termine della procedura la finestra si chiuderà automaticamente.
