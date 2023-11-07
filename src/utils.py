08616b655ca15789eb5b7d1e6ede673dac2fb9bb
Grufoony <gregorio_2000@hotmail.it>
2023-11-07 10:50:06 +0100

    working draft

--- src/utils.py
+++ src/utils.py
@@ -185,7 +185,12 @@ def print_counts(df: pd.core.frame.DataFrame) -> None:
     print("TOTALE ATLETI PARTECIPANTI: " + str(len(counter_df.index)))
 
 
-def groupdata(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+def groupdata(
+    df: pd.core.frame.DataFrame,
+    by_points: bool = False,
+    use_jolly: bool = False,
+    out_df: pd.core.frame.DataFrame = None,
+) -> pd.core.frame.DataFrame:
     """
     This function takes a dataframe as input and returns a new dataframe with the correct
     format.
@@ -200,53 +205,92 @@ def groupdata(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     pandas.core.frame.DataFrame
         The converted dataframe.
     """
-
     # keep only rows with boolean set to T (valid times) and strip spaces in style column
     df.drop(df.loc[df["Boolean"].str.strip() != "T"].index, inplace=True)
     df["Style"] = df["Style"].str.strip()
     # keeping only interesting data
-    df = df[["Name", "Year", "Sex", "Style", "Distance", "Category", "Time", "Team", "Points", "Double"]]
+    df = df[
+        [
+            "Name",
+            "Year",
+            "Sex",
+            "Style",
+            "Distance",
+            "Category",
+            "Time",
+            "Team",
+            "Points",
+            "Double",
+        ]
+    ]
     # replacing style names
     df = df.replace({"Style": STYLES})
     df["Race"] = df["Distance"].astype(str) + " " + df["Style"]
     # groupby races and times, i.e. get unique athletes
-    df = df.groupby(["Name", "Year", "Sex", "Category", "Team"])[["Race", "Time", "Points", "Double"]].agg(list)
-
-    # creates empty output database with columns' names
-    out_columns = ["Cognome", "Nome", "Anno", "Sesso"]
-    max_len = df["Race"].apply(len).max()
-    for i in range(max_len):
-        out_columns.append("Gara" + str(i + 1))
-        out_columns.append("Tempo" + str(i + 1))
-    out_columns.append("Societa")
-    out_columns.append("PuntiTotali")
-    out_df = pd.DataFrame(columns=out_columns)
-
-    out_df["Anno"] = df.index.get_level_values("Year")
-    out_df["Sesso"] = df.index.get_level_values("Sex")
-    out_df["Categoria"] = df.index.get_level_values("Category")
-    out_df["Societa"] = df.index.get_level_values("Team")
+    df = df.groupby(["Name", "Year", "Sex", "Category", "Team"])[
+        ["Race", "Time", "Points", "Double"]
+    ].agg(list)
 
     # get unique athletes
     athletes = df.index
 
-    # split name column into words and ask surname in input if the number of words
-    # is greater than 2
-    for index, full_name in enumerate(athletes.get_level_values("Name")):
-        name, surname = split_names(full_name=full_name)
-        out_df.loc[index, "Nome"] = name
-        out_df.loc[index, "Cognome"] = surname
+    if out_df is None:
+        # creates empty output database with columns' names
+        out_columns = ["Cognome", "Nome", "Anno", "Sesso"]
+        max_len = df["Race"].apply(len).max()
+        for i in range(max_len):
+            out_columns.append("Gara" + str(i + 1))
+            out_columns.append("Tempo" + str(i + 1))
+        out_columns.append("Societa")
+        out_df = pd.DataFrame(columns=out_columns)
 
-    for athlete_index, row in enumerate(df.itertuples()):
-        for race_index, race in enumerate(zip(row.Race, row.Time)):
-            out_df.loc[athlete_index, "Gara" + str(race_index + 1)] = race[0]
-            out_df.loc[athlete_index, "Tempo" + str(race_index + 1)] = race[1]
-        p = 0
-        for points, double in zip(row.Points, row.Double):
-            p += int(points)
-            if int(double) != 1000:
+        out_df["Anno"] = df.index.get_level_values("Year")
+        out_df["Sesso"] = df.index.get_level_values("Sex")
+        out_df["Societa"] = df.index.get_level_values("Team")
+
+        # split name column into words and ask surname in input if the number of words
+        # is greater than 2
+        for index, full_name in enumerate(athletes.get_level_values("Name")):
+            name, surname = split_names(full_name=full_name)
+            out_df.loc[index, "Nome"] = name
+            out_df.loc[index, "Cognome"] = surname
+
+        for athlete_index, row in enumerate(df.itertuples()):
+            for race_index, race in enumerate(zip(row.Race, row.Time)):
+                out_df.loc[athlete_index, "Gara" + str(race_index + 1)] = race[0]
+                out_df.loc[athlete_index, "Tempo" + str(race_index + 1)] = race[1]
+
+    if by_points:
+        out_df["PuntiTotali"] = 0
+        out_df["Categoria"] = df.index.get_level_values("Category")
+        for athlete_index, row in enumerate(df.itertuples()):
+            p = 0
+            for points, double in zip(row.Points, row.Double):
                 p += int(points)
-        out_df.loc[athlete_index, "PuntiTotali"] = p
+                if use_jolly and int(double) == 2:
+                    p += int(points)
+            out_df.loc[athlete_index, "PuntiTotali"] = p
+
+        # print athlete with more points for each category and sex
+        # if there are more than one athlete with the same points, print the one with lowest SL time
+
+        temp_df = out_df.copy()
+        temp_df["TempoStile"] = ""
+        for row in temp_df.itertuples():
+            for i in range(1, 5):
+                if "SL" in str(getattr(row, f"Gara{i}")):
+                    temp_df.at[row.Index, "TempoStile"] = getattr(row, f"Tempo{i}")
+                    break
+
+        temp_df = temp_df.groupby(["Categoria", "Sesso"])[
+            ["Nome", "Cognome", "PuntiTotali", "TempoStile"]
+        ].apply(
+            lambda x: x.sort_values(
+                by=["PuntiTotali", "TempoStile"], ascending=[False, True]
+            ).head(3)
+        )
+
+        print(temp_df)
 
     return out_df
 
