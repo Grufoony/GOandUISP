e0ec4c21a5174589f88cb8a93649a80782a477b1
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-11-09 15:26:46 +0100

    Add relay categories pipeline

---
+++ races/relay_categories.py
@@ -0,0 +1,25 @@
+"""
+This module contains the pipeline useful to automatically add categories to a staffette file.
+Requires the individual file to be present.
+All files must be downloaded from the UISP nazionale portal.
+"""
+
+import sys
+
+sys.path.insert(1, ".")
+# pylint: disable=wrong-import-position
+from src import go_and_uisp as GOandUISP
+
+# pylint enable=wrong-import-position
+
+__version__ = "09/11/2023"
+__author__ = "Gregorio Berselli"
+
+if __name__ == "__main__":
+    print(f"Relay Categories by {__author__}, aggiornato al {__version__}")
+    print(f"Basato su GOandUISP v{GOandUISP.__version__}\n")
+    print(
+        "Questo programma riempie automaticamente la categoria nei file staffette,"
+        + " se presente il file di iscrizioni individuali.\n"
+    )
+    GOandUISP.find_categories()
