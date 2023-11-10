42f2f450f16acbdceaeb59254febdb9e71c35829
Grufoony <gregorio_2000@hotmail.it>
2023-11-10 15:58:11 +0100

    Fix tests and update data

--- test/test_go_and_uisp.py
+++ test/test_go_and_uisp.py
@@ -61,65 +61,26 @@ def test_reformat_relay():
 
 def test_groupdata1():
     """
-    This function tests the GOandUISP.groupdata function.
-    GIVEN a dataframe
-    WHEN the function is called
-    THEN it returns a dataframe with the correct format.
-    """
-    df = pd.read_excel("datasets/reformat_test.xlsx", header=None)
-    out = GOandUISP.reformat(df)
-    out = GOandUISP.groupdata(out)
-    assert out.columns.tolist() == [
-        "Cognome",
-        "Nome",
-        "Anno",
-        "Sesso",
-        "Gara1",
-        "Tempo1",
-        "Gara2",
-        "Tempo2",
-        "Gara3",
-        "Tempo3",
-        "Societa",
-    ]
-    assert out.loc[0].tolist() == [
-        "ROSI",
-        "MARIA",
-        2011,
-        " F ",
-        "100 M",
-        " 01'26\"90 ",
-        np.nan,
-        np.nan,
-        np.nan,
-        np.nan,
-        "Catanzaro",
-    ]
-    assert out.loc[1].tolist() == [
-        "ROSSI",
-        "MARIO",
-        2013,
-        " M ",
-        "50 Delfino",
-        " 00'47\"10 ",
-        "100 Dorso",
-        " 01'24\"80 ",
-        "200 Rana",
-        " 01'24\"80 ",
-        "Aosta",
-    ]
-
-
-def test_groupdata2():
-    """
     This function tests the GOandUISP.groupdata function when the athlets have same points.
     GIVEN a dataframe
     WHEN the function is called
     THEN it returns a dataframe with the athlets sorted by time.
     """
-    df = pd.read_excel("datasets/groupby_test.xlsx", header=None)
-    df = GOandUISP.reformat(df)
-    out = GOandUISP.groupdata(df, by_points=True, use_jolly=True)
+    data = {
+        "Name": ["Rossi Mario", "Rosi Luigi", "Rossi Mario"],
+        "Year": [2010, 2010, 2010],
+        "Sex": ["M", "M", "M"],
+        "Team": ["Aosta", "Catanzaro", "Aosta"],
+        "Style": ["SL", "SL", "Delfino"],
+        "Distance": [100, 100, 100],
+        "Category": ["A", "A", "A"],
+        "Time": ["01'26\"45", "01'23\"45", "01'23\"45"],
+        "Points": [1, 1, 0],
+        "Double": ["0", "0", "0"],
+        "Boolean": ["T", "T", "T"],
+    }
+    df = pd.DataFrame(data)
+    out = GOandUISP.groupdata(df, by_points=True)
     assert out.columns.tolist() == [
         "Cognome",
         "Nome",
@@ -127,34 +88,11 @@ def test_groupdata2():
         "PuntiTotali",
         "TempoStile",
     ]
-    assert out.PuntiTotali.tolist() == [70, 70]
-    assert out.TempoStile.tolist() == [
-        " 01'22\"80 ",
-        " 01'26\"90 ",
-    ]
+    assert out.PuntiTotali.tolist() == [1, 1]
+    assert out.TempoStile.tolist() == ["01'23\"45", "01'26\"45"]
 
 
-def test_fill_categories():
-    """
-    This function tests the GOandUISP.fill_categories function.
-    GIVEN a dataframe
-    WHEN the function is called
-    THEN it returns a dataframe with the correct categories.
-    """
-    df = pd.read_csv("datasets/fill_categories-staffette.csv", sep=";")
-    df_data = pd.read_csv("datasets/fill_categories.csv", sep=";")
-
-    # glue together 'Cognome' and 'Nome' of df_data
-    df_data["Nome"] = df_data["Cognome"] + " " + df_data["Nome"]
-    # make 'Nome' column lowercase
-    df_data["Nome"] = df_data["Nome"].str.lower()
-    df_data["Nome"] = df_data["Nome"].str.strip()
-
-    out = GOandUISP.fill_categories(df, df_data)
-    assert out["CategoriaVera"].values[0] == "A"
-
-
-def test_groupdata3():
+def test_groupdata2():
     """
     This function tests the GOandUISP.groupdata function when the dataframe has only one row.
     GIVEN a dataframe with only one row
@@ -196,7 +134,7 @@ def test_groupdata3():
     ]
 
 
-def test_groupdata4():
+def test_groupdata3():
     """
     This function tests the GOandUISP.groupdata function when the dataframe has multiple rows.
     GIVEN a dataframe with multiple rows
@@ -253,7 +191,7 @@ def test_groupdata4():
     ]
 
 
-def test_groupdata5():
+def test_groupdata4():
     """
     This function tests the GOandUISP.groupdata function when by_points is True.
     GIVEN a dataframe and by_points is True
@@ -318,3 +256,23 @@ def test_print_counts(capfd):
         == "Team\nAosta        2\nCatanzaro    1\nName: count, "
         + "dtype: int64\nTOTALE ATLETI PARTECIPANTI: 3\n"
     )
+
+
+def test_fill_categories():
+    """
+    This function tests the GOandUISP.fill_categories function.
+    GIVEN a dataframe
+    WHEN the function is called
+    THEN it returns a dataframe with the correct categories.
+    """
+    df = pd.read_csv("datasets/fill_categories-staffette.csv", sep=";")
+    df_data = pd.read_csv("datasets/fill_categories.csv", sep=";")
+
+    # glue together 'Cognome' and 'Nome' of df_data
+    df_data["Nome"] = df_data["Cognome"] + " " + df_data["Nome"]
+    # make 'Nome' column lowercase
+    df_data["Nome"] = df_data["Nome"].str.lower()
+    df_data["Nome"] = df_data["Nome"].str.strip()
+
+    out = GOandUISP.fill_categories(df, df_data)
+    assert out["CategoriaVera"].values[0] == "A"
