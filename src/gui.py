4f8cb037c2352dc2101ba7ce0709adf42d5efc38
Grufoony <64806874+Grufoony@users.noreply.github.com>
2024-05-01 13:58:36 +0200

    Add gui for print counts

--- src/gui.py
+++ src/gui.py
@@ -1,25 +1,92 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 import pandas as pd
 import go_and_uisp as go
 
 
+class DataFrameWindow(tk.Toplevel):  # Inherits from tk.Toplevel
+
+    def __init__(
+        self, your_dataframe, title
+    ):  # the dataframe you passed through is here
+        super().__init__()
+
+        # Frame for TreeView
+        frame = tk.LabelFrame(self, text="DataFrame")
+        frame.pack(fill="both", expand="true")
+        frame.pack_propagate(0)
+
+        # the size of the window.
+        self.geometry("1280x720")
+        # self.resizable(0, 0)
+        self.title(title)  # the window title
+
+        # This creates your Treeview widget.
+        tv1 = ttk.Treeview(frame)
+        tv1.place(
+            relheight=1, relwidth=1
+        )  # set the height and width of the widget to 100% of its container (frame).
+
+        treescrolly = tk.Scrollbar(
+            frame, orient="vertical", command=tv1.yview
+        )  # command means update the yaxis view of the widget
+        treescrollx = tk.Scrollbar(
+            frame, orient="horizontal", command=tv1.xview
+        )  # command means update the xaxis view of the widget
+        tv1.configure(
+            xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set
+        )  # assign the scrollbars to the Treeview Widget
+        treescrollx.pack(
+            side="bottom", fill="x"
+        )  # make the scrollbar fill the x axis of the Treeview widget
+        treescrolly.pack(
+            side="right", fill="y"
+        )  # make the scrollbar fill the y axis of the Treeview widget
+
+        # add also index
+        your_dataframe.reset_index(inplace=True)
+        your_dataframe.rename(columns={"index": "Indice"}, inplace=True)
+
+        # this loads the dataframe into the treeview widget
+        tv1["column"] = list(your_dataframe.columns)
+        tv1["show"] = "headings"
+        for column in tv1["columns"]:
+            tv1.heading(column, text=column)  # let the column heading = column name
+
+        df_rows = (
+            your_dataframe.to_numpy().tolist()
+        )  # turns the dataframe into a list of lists
+        for row in df_rows:
+            tv1.insert(
+                "", "end", values=row
+            )  # inserts each list into the treeview. For parameters see https://docs.python.org/3/library/tkinter.ttk.html#tkinter.ttk.Treeview.insert
+
+
 class GUI:
+    __version__ = (0, 1, 0)
+    __author__ = "Grufoony"
+
     def __init__(self):
+        self.cached_df = None
+
         self.mainWin = tk.Tk()
-        self.mainWin.title("Go And Uisp v" + go.version())
+        self.mainWin.title("Swim Utils v" + ".".join(map(str, self.__version__)))
         self.mainWin.minsize(width=500, height=300)
 
         self.menuBar = tk.Menu(self.mainWin)
 
         # File menu
         self.fileMenu = tk.Menu(self.menuBar, tearoff=0)
-        self.fileMenu.add_command(label="Exit", command=self.mainWin.quit)
         self.fileMenu.add_command(label="Open", command=self.openFile)
         self.menuBar.add_cascade(label="File", menu=self.fileMenu)
         # add about menu
         self.aboutMenu = tk.Menu(self.menuBar, tearoff=0)
-        self.aboutMenu.add_command(label="About", command=lambda: print("About"))
+        self.aboutMenu.add_command(
+            label="About",
+            command=lambda: messagebox.showinfo(
+                "About", "Go And Uisp v" + go.version()
+            ),
+        )
         self.menuBar.add_cascade(label="About", menu=self.aboutMenu)
         # add help menu
         self.helpMenu = tk.Menu(self.menuBar, tearoff=0)
@@ -28,8 +95,10 @@ class GUI:
 
         self.mainWin.config(menu=self.menuBar)
 
-        # self.youngchallengeButton = tk.Button(text="Young Challenge", command=lambda : go.accumulate(points=True, jolly=True))
-        # self.youngchallengeButton.pack()
+        self.youngchallengeButton = tk.Button(
+            text="Print Counts", command=lambda: self.printCounts()
+        )
+        self.youngchallengeButton.pack()
 
         # self.combinedButton = tk.Button(text="Combinata", command=lambda : go.accumulate())
         # self.combinedButton.pack()
@@ -41,18 +110,27 @@ class GUI:
         self.file = ""
         while True:
             self.file = filedialog.askopenfilename()
-            if(type(self.file) == tuple):
+            if type(self.file) == tuple:
                 break
             print(type(self.file))
             print(self.file)
             try:
-                df = pd.read_csv(self.file)
+                if self.file.endswith(".csv"):
+                    df = pd.read_csv(self.file, header=None)
+                elif self.file.endswith(".xlsx"):
+                    df = pd.read_excel(self.file, header=None)
                 self.cached_df = go.reformat(df)
                 break
-            except:
+            except Exception as e:
                 if self.file == "":
                     break
-                messagebox.showerror("Error", "Invalid file")
+                messagebox.showerror("Invalid File", f"Error details: {e.__context__}")
+
+    def printCounts(self):
+        if self.cached_df is None:
+            messagebox.showerror("No File", "No file has been loaded yet.")
+        # set filename as title
+        DataFrameWindow(go.get_counts(self.cached_df), title=self.file)
 
 
 if __name__ == "__main__":
