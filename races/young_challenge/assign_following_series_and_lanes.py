fd20cabc701c0670c6042851018cb0d866048cfb
Grufoony <gregorio_2000@hotmail.it>
2024-11-03 10:43:29 +0100

    Update script

--- races/young_challenge/assign_following_series_and_lanes.py
+++ races/young_challenge/assign_following_series_and_lanes.py
@@ -17,24 +17,28 @@ __version__ = "2024.11.02"
 __author__ = "Gregorio Berselli"
 
 if __name__ == "__main__":
-    print(f"ASSIGN FOLLOWING SERIES AND LANES by {__author__}, aggiornato al {__version__}")
+    print(
+        f"ASSIGN FOLLOWING SERIES AND LANES by {__author__}, aggiornato al {__version__}"
+    )
     print(f"Basato su GOandUISP v{GOandUISP.__version__}\n")
     print(
         'Questo programma è stato creato per la manifestazione "YOUNG CHALLENGE", '
         "come utility per l'assegnazione delle batterie nelle gare a inseguimento.\n"
     )
-    RESPONSE = filedialog.askopenfilename(
+    file_name = filedialog.askopenfilename(
         title="Seleziona il file CSV da cui leggere i dati",
         filetypes=[("CSV files", "*.csv")],
     )
-    if not RESPONSE:
+    if not file_name:
         print("Nessun file selezionato, esco.")
         input("Premi un tasto qualsiasi per chiudere...")
         sys.exit(0)
-    df = pd.read_csv(RESPONSE, sep=";")
-    RESPONSE = int(input("Inserisci il seed per la generazione casuale: "))
-    np.random.seed(RESPONSE)
-    df = GOandUISP.assing_random_series_and_lanes(df, 7)
+    df = pd.read_csv(file_name, sep=";")
+    seed = int(input("Inserisci il seed per la generazione casuale: "))
+    np.random.seed(seed)
+    n_lower_lane = int(input("Inserisci il numero della corsia inferiore: "))
+    n_series = int(input("Inserisci il numero della prima serie: "))
+    df = GOandUISP.assing_random_series_and_lanes(df, 7, n_lower_lane, n_series)
 
     df.to_csv("./batterie_inseguimento.csv", sep=";", index=False)
     print("File batterie_inseguimento.csv salvato correttamente.")
