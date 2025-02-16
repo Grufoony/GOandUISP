297627796faf307b82697abd26a8341b23c04dde
grufoony <gregorio_2000@hotmail.it>
2025-02-16 19:22:42 +0100

    Simplify installation

--- build.bat
+++ build.bat
@@ -1,16 +1,6 @@
 @echo off
-set /p buildType="Enter the build type (bsl - sonopronto - youngchallenge): "
 
 pip install .
+python -m PyInstaller --noconsole --onefile .\app\app.py
 
-if /I "%buildType%"=="bsl" (
-    python -m PyInstaller --onefile .\races\bsl\bsl.py
-    python -m PyInstaller --onefile .\races\bsl\ranker.py
-) else if /I "%buildType%"=="sonopronto" (
-    python -m PyInstaller --onefile .\races\sono_pronto\sono_pronto.py
-) else if /I "%buildType%"=="youngchallenge" (
-    python -m PyInstaller --onefile .\races\young_challenge\young_challenge.py
-) else (
-    echo Unknown build type: %buildType%
-)
 pause
